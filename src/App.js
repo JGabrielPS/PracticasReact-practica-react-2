d28ccaf6af8b228178ff4ba019f6871cd45193e9
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-08 16:15:12 -0300

    addition: added Bienvenida and Card components to practice props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import ListadoNombres from "./components/ListadoNombres";
+import Bienvenida from "./components/Bienvenida";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="container">
       <ListadoNombres />
+      <Bienvenida nombre="Manuel" />
+      <Bienvenida nombre="Jose" />
+      <Bienvenida nombre="Susanita" />
+      <Bienvenida nombre="Pepe" />
+      <div className="row">
+        <div className="col">
+          <Card
+            imagen="https://picsum.photos/100"
+            titulo="Título de card 1"
+            texto="Texto de la card 1"
+          />
+        </div>
+        <div className="col">
+          <Card
+            imagen="https://picsum.photos/100"
+            titulo="Título de card 2"
+            texto="Texto de la card 2"
+          />
+        </div>
+        <div className="col">
+          <Card
+            imagen="https://picsum.photos/100"
+            titulo="Título de card 3"
+            texto="Texto de la card 3"
+          />
+        </div>
+      </div>
     </div>
   );
 }
